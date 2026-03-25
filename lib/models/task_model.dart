d8703c54c7a238c91f1a7d9c8a12ccb9b7cb3557
Semiha BAHÇEBAŞI <162346635+semihabahcebasi@users.noreply.github.com>
2026-03-26 00:49:33 +0300

    görevleri listeleme ve checkbox tasarımına başlandı

---
+++ lib/models/task_model.dart
@@ -0,0 +1,7 @@
+class TaskModel {
+  String name;
+  bool isDone;
+
+  //constracture
+  TaskModel({required this.name, this.isDone = false});
+}
--- lib/screens/daily_tasks.dart
+++ lib/screens/daily_tasks.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:study_map_app/models/task_model.dart';
 
-class DailyTasks extends StatelessWidget {
+class DailyTasks extends StatefulWidget {
   const DailyTasks({super.key});
+
+  @override
+  State<DailyTasks> createState() => _DailyTasksState();
+}
+
+class _DailyTasksState extends State<DailyTasks> {
+  // check box için göreverin tutalacağı liste tanımlandı
+  List<TaskModel> dailyTasks = [];
+
+  // yeni görev ekleme fonksiyonu
+  void newTask(String taskName) {
+    setState(() {});
+    // listeye yeni eleman eklemek için list.add metodu kullanılır
+    dailyTasks.add(TaskModel(name: taskName));
+  }
+
+  // görev ekleme pencerresi
+  //*************************** */
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +41,41 @@ class DailyTasks extends StatelessWidget {
             fontWeight: FontWeight(900),
           ),
         ),
+      ),
+
+      body: ListView.builder(
+        itemCount: dailyTasks.length, // liste uzunluğu kadar eleman çizsin
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            title: Text(
+              dailyTasks[index].name,
+              style: TextStyle(
+                // tamamlanan görevlerin üzerini çizmek için decoration
+                decoration: dailyTasks[index].isDone
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+
+            //buradaki value checkboxlisttitle ın parametresidir
+            value: dailyTasks[index].isDone,
+
+            // buradaki value ise fonksiyon değişkenidir
+            onChanged: (bool? value) {
+              setState(() {
+                dailyTasks[index].isDone = value!;
+              });
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black26,
+        onPressed: () {
+          //tıklandığında görev ekleme penceresi açılır
+        },
+        child: Icon(Icons.add, color: Colors.white),
       ),
     );
   }
