c84cfa3c8c74d95f450da29052759f976289d1f1
Semiha BAHÇEBAŞI <162346635+semihabahcebasi@users.noreply.github.com>
2026-04-03 02:05:06 +0300

    verilerin silinmesi probleminin çözümü

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -2,6 +2,13 @@ class TaskModel {
   String name;
   bool isDone;
 
-  //constracture
   TaskModel({required this.name, this.isDone = false});
+
+  // Modeli Map'e (sözlüğe) çevirir
+  Map<String, dynamic> toMap() => {'name': name, 'isDone': isDone};
+
+  // Map'ten model oluşturur
+  factory TaskModel.fromMap(Map<String, dynamic> map) =>
+      TaskModel(name: map['name'], isDone: map['isDone']);
 }
+//Bu işlem, modeli metne dönüştürmeye yarar):
